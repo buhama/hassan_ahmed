b7b41a3227c48b0756b887761bc71d8a42ce4705
buhama <hassan@hassanahmed.ca>
2021-12-07 21:59:05 -0500

    Projects can now scroll properly without entire page scrolling

--- src/components/project.js
+++ src/components/project.js
@@ -1,5 +1,89 @@
+import { Fragment } from 'react';
+import { Box } from '@chakra-ui/layout';
+import { Image } from '@chakra-ui/image';
+import photo from '../images/photo.jpg';
+
 const project = () => {
-  return <div></div>;
+  return (
+    <Fragment>
+      {' '}
+      <Box display='flex' bg='#f9f9f9' p={4} borderRadius='lg'>
+        <Image
+          borderRadius='full'
+          boxSize='90px'
+          src={photo}
+          alt='Hassan Ahmed'
+        />
+        <Box>
+          <h1 className='project-title'>To The Person Reading This</h1>
+          <p className='project-description'>
+            Some description of the project, like damn i guess it was cool or
+            something idk, maybe it was, maybe it wasn't.{' '}
+          </p>
+        </Box>
+      </Box>
+      <Box display='flex' bg='#f9f9f9' p={4} my={4} borderRadius='lg'>
+        <Image
+          borderRadius='full'
+          boxSize='90px'
+          src={photo}
+          alt='Hassan Ahmed'
+        />
+        <Box>
+          <h1 className='project-title'>To The Person Reading This</h1>
+          <p className='project-description'>
+            Some description of the project, like damn i guess it was cool or
+            something idk, maybe it was, maybe it wasn't.{' '}
+          </p>
+        </Box>
+      </Box>
+      <Box display='flex' bg='#f9f9f9' p={4} my={4} borderRadius='lg'>
+        <Image
+          borderRadius='full'
+          boxSize='90px'
+          src={photo}
+          alt='Hassan Ahmed'
+        />
+        <Box>
+          <h1 className='project-title'>To The Person Reading This</h1>
+          <p className='project-description'>
+            Some description of the project, like damn i guess it was cool or
+            something idk, maybe it was, maybe it wasn't.{' '}
+          </p>
+        </Box>
+      </Box>
+      <Box display='flex' bg='#f9f9f9' p={4} my={4} borderRadius='lg'>
+        <Image
+          borderRadius='full'
+          boxSize='90px'
+          src={photo}
+          alt='Hassan Ahmed'
+        />
+        <Box>
+          <h1 className='project-title'>To The Person Reading This</h1>
+          <p className='project-description'>
+            Some description of the project, like damn i guess it was cool or
+            something idk, maybe it was, maybe it wasn't.{' '}
+          </p>
+        </Box>
+      </Box>
+      <Box display='flex' bg='#f9f9f9' p={4} my={4} borderRadius='lg'>
+        <Image
+          borderRadius='full'
+          boxSize='90px'
+          src={photo}
+          alt='Hassan Ahmed'
+        />
+        <Box>
+          <h1 className='project-title'>To The Person Reading This</h1>
+          <p className='project-description'>
+            Some description of the project, like damn i guess it was cool or
+            something idk, maybe it was, maybe it wasn't.{' '}
+          </p>
+        </Box>
+      </Box>
+    </Fragment>
+  );
 };
 
 export default project;
--- src/components/projects.js
+++ src/components/projects.js
@@ -2,45 +2,26 @@ import { Fragment } from 'react';
 import photo from '../images/photo.jpg';
 import { Box } from '@chakra-ui/layout';
 import { Image } from '@chakra-ui/image';
+import Project from './project';
 
 const projects = () => {
   return (
-    <div>
-      <section className='projects'>
-        <Box borderWidth='1px' borderRadius='lg' overflow='hidden' p={4}>
-          <Box display='flex' bg='#f9f9f9' p={4} borderRadius='lg'>
-            <Image
-              borderRadius='full'
-              boxSize='90px'
-              src={photo}
-              alt='Hassan Ahmed'
-            />
-            <Box>
-              <h1 className='project-title'>To The Person Reading This</h1>
-              <p className='project-description'>
-                Some description of the project, like damn i guess it was cool
-                or something idk, maybe it was, maybe it wasn't.{' '}
-              </p>
-            </Box>
-          </Box>
-          <Box display='flex' bg='#f9f9f9' p={4} my={4} borderRadius='lg'>
-            <Image
-              borderRadius='full'
-              boxSize='90px'
-              src={photo}
-              alt='Hassan Ahmed'
-            />
-            <Box>
-              <h1 className='project-title'>To The Person Reading This</h1>
-              <p className='project-description'>
-                Some description of the project, like damn i guess it was cool
-                or something idk, maybe it was, maybe it wasn't.{' '}
-              </p>
-            </Box>
-          </Box>
-        </Box>
-      </section>
-    </div>
+    <Box
+      borderWidth='1px'
+      borderRadius='lg'
+      p={4}
+      mx={6}
+      className='projects-box'
+    >
+      <div className='projects'>
+        <div className='projects-header'>
+          <h1>Projects</h1>
+        </div>
+        <div className='project'>
+          <Project />
+        </div>
+      </div>
+    </Box>
   );
 };
 
