6a135f9069a0febd491e8b60bed50b10cc12127b
buhama <hassan@hassanahmed.ca>
2021-12-13 17:01:26 -0500

    replaced contact button with social icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,15 @@
-import { Box, Flex, Link, Spacer, useColorMode } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Link,
+  Spacer,
+  useColorMode,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import { ArrowForwardIcon, SunIcon } from '@chakra-ui/icons';
 import { Button } from '@chakra-ui/button';
+import { IconButton, SimpleGrid } from '@chakra-ui/react';
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 import { Link as RouterLink } from 'react-router-dom';
 
@@ -17,15 +26,40 @@ const Header = () => {
         </div>
         <Spacer />
         <div className='nav-links'>
-          <Button
+          {/* <Button
             rightIcon={<ArrowForwardIcon />}
             colorScheme='blue'
             variant='solid'
             size='sm'
             mr='4'
-          >
+          > 
             Contact{' '}
-          </Button>
+          </Button>*/}
+          <Link href='https://github.com/buhama' isExternal pr={2}>
+            <IconButton
+              size='sm'
+              aria-label='Github Link'
+              icon={<FaGithub />}
+            />
+          </Link>
+          <Link href='https://twitter.com/hassanahmed_ca' isExternal pr={2}>
+            <IconButton
+              size='sm'
+              aria-label='Twitter Link'
+              icon={<FaTwitter />}
+            />
+          </Link>
+          <Link
+            href='https://www.linkedin.com/in/hassan-ahmed-751255131/'
+            isExternal
+            pr={2}
+          >
+            <IconButton
+              size='sm'
+              aria-label='LinkedIn Link'
+              icon={<FaLinkedin />}
+            />
+          </Link>
           <Button
             onClick={toggleColorMode}
             rightIcon={<SunIcon />}
