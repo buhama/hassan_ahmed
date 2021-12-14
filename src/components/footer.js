c5c255db4a41997b35433259d705db00dd7d9209
buhama <hassan@hassanahmed.ca>
2021-12-14 02:16:28 -0500

    added email to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import { Link, Box, useColorModeValue } from '@chakra-ui/react';
-import { IconButton, SimpleGrid } from '@chakra-ui/react';
+import { IconButton, SimpleGrid, Center } from '@chakra-ui/react';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
 const Footer = () => {
@@ -7,38 +7,53 @@ const Footer = () => {
   const twitterColor = useColorModeValue('blue', 'blue');
   const githubColor = useColorModeValue('green', 'red');
   const linkedinColor = useColorModeValue('red', 'purple');
+  const linkColor = useColorModeValue('blue.900', 'blue.100');
 
   return (
     <Box bg={bg}>
       <div className='footer'>
-        <div className='icons'>
-          <SimpleGrid columns={3} spacing={12}>
-            <Link href='https://github.com/buhama' isExternal>
-              <IconButton
-                aria-label='Github Link'
-                colorScheme={githubColor}
-                icon={<FaGithub />}
-              />
-            </Link>
-            <Link href='https://twitter.com/hassanahmed_ca' isExternal>
-              <IconButton
-                aria-label='Twitter Link'
-                colorScheme={twitterColor}
-                icon={<FaTwitter />}
-              />
-            </Link>
-            <Link
-              href='https://www.linkedin.com/in/hassan-ahmed-751255131/'
-              isExternal
-            >
-              <IconButton
-                aria-label='LinkedIn Link'
-                colorScheme={linkedinColor}
-                icon={<FaLinkedin />}
-              />
-            </Link>
-          </SimpleGrid>
-        </div>
+        <Center>
+          <div className='icons-container'>
+            <div className='icons'>
+              <SimpleGrid columns={3} spacing={1}>
+                <Link href='https://github.com/buhama' isExternal>
+                  <IconButton
+                    aria-label='Github Link'
+                    colorScheme={githubColor}
+                    icon={<FaGithub />}
+                  />
+                </Link>
+                <Link href='https://twitter.com/hassanahmed_ca' isExternal>
+                  <IconButton
+                    aria-label='Twitter Link'
+                    colorScheme={twitterColor}
+                    icon={<FaTwitter />}
+                  />
+                </Link>
+                <Link
+                  href='https://www.linkedin.com/in/hassan-ahmed-751255131/'
+                  isExternal
+                >
+                  <IconButton
+                    aria-label='LinkedIn Link'
+                    colorScheme={linkedinColor}
+                    icon={<FaLinkedin />}
+                  />
+                </Link>
+              </SimpleGrid>
+            </div>
+          </div>
+        </Center>
+        <Center>
+          <Link
+            href='mailto:hassan@hassanahmed.ca'
+            isExternal
+            color={linkColor}
+            style={{ textDecoration: 'underline', fontWeight: 'bold' }}
+          >
+            <p>hassan@hassanahmed.ca</p>
+          </Link>
+        </Center>
       </div>
     </Box>
   );
